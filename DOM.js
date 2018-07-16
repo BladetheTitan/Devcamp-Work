a85544b5b649939c6e6551419ccbc56b66c29ec8
Evan Norland <bladethetitan@gmail.com>
2018-07-16 14:30:13 +0000

    styled input grid

---
+++ DOM.js
@@ -0,0 +1,12 @@
+// DOM -> Document Object Model
+
+const user = {
+    name: 'Evan',
+    email: 'BladetheTitan@gmail.com',
+    favorites: [
+        'Whistle Wok',
+        'Chik fil a'
+    ]
+}
+
+user.email
